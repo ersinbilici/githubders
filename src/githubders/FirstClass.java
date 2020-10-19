d8c779a901c9d0015ad0234ea77c5c970ae91ed9
ersinbilici <67049444+ersinbilici@users.noreply.github.com>
2020-10-19 21:30:21 +0200

    faruk beyden

--- src/githubders/FirstClass.java
+++ src/githubders/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Saat 20:24");
 		System.out.println("Saat 21:21");
+		System.out.println("Saat 21:28");
 
 	}
 
